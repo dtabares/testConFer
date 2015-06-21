e6444ff1714577ca85cead9d5af8dc9307e21a3e
Fernando Ares <ferares4@gmail.com>
2015-06-21 18:56:43 -0300

    Separo clase GeneradorDeEstadisticas en clases independientes

--- pruebas/src/Recorrido.java
+++ pruebas/src/Recorrido.java
@@ -1,41 +1,45 @@
-import java.awt.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.Locale;
 
 
 public class Recorrido {
 
-	//usuarioid	bicicletaid	origenfecha	origenestacionid	origennombre	destinofecha	destinoestacionid	destinonombre	tiempouso
 
-
-	private int usuarioid;
-	private int bicicletaid;
+	private String usuarioid;
+	private String bicicletaid;
     private GregorianCalendar origenfecha;
-    private int origenestacionid;
+    private String origenestacionid;
     private String origennombre;
     private GregorianCalendar destinofecha;
-    private int destinoestacionid;
+    private String destinoestacionid;
     private String destinonombre;
-    private String tiempouso;
-
-
-
+    private int tiempouso;
+    
     public Recorrido(String usuarioid, String bicicletaid, String origenfecha, String origenestacionid, String origennombre, String destinofecha,String destinoestacionid,String destinonombre, String tiempouso) {
-		super();
-		this.usuarioid = Integer.parseInt(usuarioid);
-		this.bicicletaid = Integer.parseInt(bicicletaid);
-		this.origenfecha = this.convertDateStringToGregorianCalendar(origenfecha) ;
-		this.origenestacionid = Integer.parseInt(origenestacionid);
+
+		this.usuarioid = usuarioid;
+		this.bicicletaid = bicicletaid;
+		this.origenfecha = this.convertDateStringToGregorianCalendar(origenfecha);
+		this.origenestacionid = origenestacionid;
 		this.origennombre = origennombre;
 		this.destinofecha = this.convertDateStringToGregorianCalendar(destinofecha);
-		this.destinoestacionid = Integer.parseInt(destinoestacionid);
+		this.destinoestacionid = destinoestacionid;
 		this.destinonombre = destinonombre;
-		this.tiempouso = tiempouso;
+		
+		if (tiempouso!=null){
+			
+			this.tiempouso = Integer.parseInt(tiempouso);
+		}
+		
+		else {
+			
+			this.tiempouso = 0;
+			
+		}
+		
 
     }
     
@@ -52,59 +56,43 @@ public class Recorrido {
 		cal.setTime(date);
 		return cal;
 	}
-
+	
 
     // Getters & Setters
 
-	public int getUsuarioid() {
+	public String getUsuarioid() {
 		return usuarioid;
 	}
 
-
-
-	public int getBicicletaid() {
+	public String getBicicletaid() {
 		return bicicletaid;
 	}
-
-
 
 	public GregorianCalendar getOrigenfecha() {
 		return origenfecha;
 	}
 
-
-
-	public int getOrigenestacionid() {
+	public String getOrigenestacionid() {
 		return origenestacionid;
 	}
-
-
 
 	public String getOrigennombre() {
 		return origennombre;
 	}
 
-
-
 	public GregorianCalendar getDestinofecha() {
 		return destinofecha;
 	}
 
-
-
-	public int getDestinoestacionid() {
+	public String getDestinoestacionid() {
 		return destinoestacionid;
 	}
-
-
 
 	public String getDestinonombre() {
 		return destinonombre;
 	}
 
-
-
-	public String getTiempouso() {
+	public int getTiempouso() {
 		return tiempouso;
 	}
 
